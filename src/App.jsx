776f58b6984eddf6a1ec43574b71e9070e048b94
Bilal Arshad Tarar <bilal.tarar008@gmail.com>
2025-11-12 14:31:23 +0500

    update the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // src/App.jsx
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import ContactUs from "./pages/Contact";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Products from "./pages/Products";
+// import ContactUs from "./pages/Contact";
 
-export default function App(){
-  return (
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/products" element={<Products/>} />
-          <Route path="/contact" element={<ContactUs/>} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-}
+// export default function App(){
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Home/>} />
+//           <Route path="/about" element={<About/>} />
+//           <Route path="/products" element={<Products/>} />
+//           <Route path="/contact" element={<ContactUs/>} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
